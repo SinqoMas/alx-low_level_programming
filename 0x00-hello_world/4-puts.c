81a69abaf839dec3311da8b4cd9f45280cff2282
Lisa <sinqomas@gmail.com>
2023-04-20 13:40:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <studio.h>
-/* The processor */
-int main(void) {
-/* Main method */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void) 
+{
   puts("\"Programming is like building a multilingual puzzle")
-/* Statement */
 return(0);
-/* return value of zero */
 }
