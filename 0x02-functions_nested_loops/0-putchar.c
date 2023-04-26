6afecda91f035149e3d01335786784b15d29c27d
Lisa <sinqomas@gmail.com>
2023-04-26 12:07:39 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints putchar in lower case
+ * returns 0
+ */
+int main(void)
+{
+	char letters = "_putchar";
+	int x;
+	for (c = 0; c < 8; c++)
+	{
+		_putchhar(abe[x]);
+	}
+	_putchar('\n);
+	return(0);
+}
